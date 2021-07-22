86b1046742bf36b3c190934f0df04eb8155cd1ef
danil chernikh <11051969dahul@mail.ru>
2021-07-22 12:53:30 +0500

    refactoring

---
+++ Byndyusoft.ValidEnumConverter/ValidEnumConverterJsonConvertersExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Byndyusoft.ValidEnumConverter
+{
+    using System.Collections.Generic;
+    using System.Text.Json.Serialization;
+    using ConverterFactory;
+    using System.Text.Json;
+
+    public static class ValidEnumConverterJsonConvertersExtensions
+    {
+        public static void AddValidEnumConverter(this IList<JsonConverter> converters)
+        {
+            converters.Add(new JsonValidEnumConverterFactory(new JsonStringEnumConverter(null, true )));
+        }
+
+        public static void AddValidEnumConverter(
+            this IList<JsonConverter> converters,
+            JsonNamingPolicy? namingPolicy = null,
+            bool allowIntegerValues = true)
+        {
+            converters.Add(
+                new JsonValidEnumConverterFactory(new JsonStringEnumConverter(namingPolicy, allowIntegerValues)));
+        }
+    }
+}
